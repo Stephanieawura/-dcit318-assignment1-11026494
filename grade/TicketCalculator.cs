c1b6a25062d20c391aaff3195002bcfaf4ef6bbe
Stephanie Awurabena <stephanieawurabena99@gmail.com>
2025-08-04 16:06:31 +0000

    Add TicketCalculator the second Task

---
+++ grade/TicketCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        int price;
+
+        if (age <= 12 || age >= 65)
+            price = 7;
+        else
+            price = 10;
+
+        Console.WriteLine($"Your ticket price is: GHC{price}");
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
