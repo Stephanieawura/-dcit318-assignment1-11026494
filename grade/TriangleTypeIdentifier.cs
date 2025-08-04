0a7f6b6db0e383354a98b77037e78987310a438b
Stephanie Awurabena <stephanieawurabena99@gmail.com>
2025-08-04 16:12:02 +0000

    Add the last tast , which is  TriangleTypeIdentifier.cs

---
+++ grade/TriangleTypeIdentifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main()
+    {
+        Console.Write("Enter side 1: ");
+        int side1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter side 2: ");
+        int side2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter side 3: ");
+        int side3 = Convert.ToInt32(Console.ReadLine());
+
+        
+        if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+        {
+            if (side1 == side2 && side2 == side3)
+                Console.WriteLine("The triangle is Equilateral.");
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+                Console.WriteLine("The triangle is Isosceles.");
+            else
+                Console.WriteLine("The triangle is Scalene.");
+        }
+        else
+        {
+            Console.WriteLine("The sides do not form a valid triangle.");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
